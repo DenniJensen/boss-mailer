42e3c0aaad8a6a82039d20a5e36afc4a27132a93
Dennis Haegler <d.haegler@hotmail.de>
2015-07-27 12:46:30 +0200

    prepare mailer

--- lib/boss_mailer/mailer.rb
+++ lib/boss_mailer/mailer.rb
@@ -3,17 +3,24 @@ module BossMailer
     def initialize(mail_settings, working_hours)
       @mail_settings = mail_settings
       @working_hours = working_hours
-      binding.pry
+      body_content = mail_body
       @mailer = Mail.new do
         to mail_settings[:to]
         from mail_settings[:from]
         subject mail_settings[:subject]
+        body body_content
       end
     end
 
     def mail
       @mailer.delivery_method :smtp, address: "localhost", port: 1025
       @mailer.deliver
+    end
+
+    def mail_body
+      "Start #{@working_hours[:start]} \n\
+      Ende #{@working_hours[:end]}\n\
+      Pause #{@working_hours[:pause]}"
     end
   end
 end
