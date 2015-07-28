76aaefc12c9ce85d02712b4ef8635879d5f520f5
Dennis Haegler <d.haegler@hotmail.de>
2015-07-28 21:49:45 +0200

    add config as delivering service, fixes #6

---
+++ lib/boss_mailer/config_deliverer.rb
@@ -0,0 +1,21 @@
+module BossMailer
+  class ConfigDeliverer
+    def self.config
+      {
+        mailer_settings: {
+          from: '',
+          to: '',
+          subject: '',
+          options: {
+            address: "smtp.gmail.com",
+            port: 587,
+            user_name: "<username>",
+            password: "<password>",
+            authentication: "plain",
+            enable_starttls_auto: true
+          }
+        }
+      }
+    end
+  end
+end
