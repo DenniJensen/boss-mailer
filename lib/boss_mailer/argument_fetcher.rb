ac83e20768491ef85a36eccf93b7c489e80d55e6
Dennis Haegler <d.haegler@hotmail.de>
2015-08-08 18:38:35 +0200

    Refactor form bin to classes

--- lib/boss_mailer/argument_fetcher.rb
+++ lib/boss_mailer/argument_fetcher.rb
@@ -1,5 +1,16 @@
 module BossMailer
   class ArgumentFetcher
-
+    def options
+      options = {}
+      OptionParser.new do |opts|
+        opts.on('--init', 'inialize empty setting in home dir') do
+          options[:init] = true
+        end
+        opts.on('--force', 'forces to create a new config file') do
+          options[:force] = true
+        end
+      end.parse!
+      options
+    end
   end
 end
--- lib/boss_mailer/config_fetcher.rb
+++ lib/boss_mailer/config_fetcher.rb
@@ -2,30 +2,36 @@ require 'yaml'
 
 module BossMailer
   class ConfigFetcher
-    attr_accessor :mail_settings
-    attr_accessor :working_hours
+    attr_reader :mail_settings
+
+    CONFIG_PATH = ENV['HOME'] + '/.boss-mailer.yml'
 
     def initialize
       @mail_settings = fetch_mail_settings
-      @working_hours = fetch_working_hours
+    end
+
+    def existing_config?
+      File.exist? CONFIG_PATH
+    end
+
+    def config_complete?
+      mail_setting_complete?
     end
 
     private
+
+    def mail_setting_complete?
+      blank = @mail_settings[:to].blank? || @mail_settings[:from].blank? ||
+              @mail_settings[:subject].blank?
+      !blank
+    end
 
     def fetch_mail_settings
       settings[:mail_settings]
     end
 
-    def fetch_working_hours
-      settings[:working_hours]
-    end
-
     def settings
-      YAML.load_file(config_path)
-    end
-
-    def config_path
-      'not yet implemented'
+      YAML.load_file(CONFIG_PATH)
     end
   end
 end
